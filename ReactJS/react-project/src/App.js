ac0185151f0dfde908f1921d323fe3f96e7bf1d5
watching-dev <coderefactoring90@gmail.com>
2024-06-21 19:16:00 +0900

    [feat] Hello 컴포넌트를 showing에 따라서 출력하는 기능 추가_5 of 70_24.06.21

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  return <h1>hello</h1>;
+}
+
 function App() {
   const [showing, setShowing] = useState(false);
   const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div>
+      {showing ? <Hello /> : null}
       <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
     </div>
   );
