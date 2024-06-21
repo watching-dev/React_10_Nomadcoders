aeb48901529ea3d8d5fce2618c1a6de4a6d02d9d
watching-dev <coderefactoring90@gmail.com>
2024-06-21 14:36:07 +0900

    [feat] App.module.css 수정하고 App.js 에 추가하여 사용_5 of 70_24.06.21

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -1,9 +1,10 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>Welcome!</h1>
+      <h1 className={styles.title}>Welcome!</h1>
       <Button text={"Continue"} />
     </div>
   );
