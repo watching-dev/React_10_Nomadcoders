bcbcd27d82cf919098468dd8200334c0d8df7743
watching-dev <coderefactoring90@gmail.com>
2024-06-21 16:52:35 +0900

    [feat] 새로운 useState, input 추가_5 of 70_24.06.21

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -4,13 +4,21 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyWord, setKeyWord] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyWord(event.target.value);
   const onlyOnce = () => {
     console.log("only once");
   };
   useEffect(onlyOnce, []);
   return (
     <div>
+      <input
+        value={keyWord}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
