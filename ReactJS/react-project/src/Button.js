06cc541132ae407efbcbb1529dd697138e78d2bf
watching-dev <coderefactoring90@gmail.com>
2024-06-21 14:21:05 +0900

    [feat] 기존 css 추가는 전체 global적으로 적용하는 거라 한 파일에 다 쓰게되면 엄청 거대해 짐, 일단 버튼에 직접적으로 자바스크립트로 적용_5 of 70_24.06.21

--- ReactJS/react-project/src/Button.js
+++ ReactJS/react-project/src/Button.js
@@ -1,7 +1,16 @@
 import PropTypes from "prop-types";
 
 function Button({ text }) {
-  return <button>{text}</button>;
+  return (
+    <button
+      style={{
+        backgroundColor: "tomato",
+        color: "white",
+      }}
+    >
+      {text}
+    </button>
+  );
 }
 
 Button.propTypes = {
