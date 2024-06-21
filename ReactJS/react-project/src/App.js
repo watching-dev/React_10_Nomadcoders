ab6698ec910e860db2f60c4fa55eac11cf2a13cb
watching-dev <coderefactoring90@gmail.com>
2024-06-21 16:21:58 +0900

    [feat] 기존 코드 useState 추가해서 수정_5 of 70_24.06.21

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -3,11 +3,12 @@ import styles from "./App.module.css";
 import { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState();
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
-      <h1 className={styles.title}>Welcome!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
