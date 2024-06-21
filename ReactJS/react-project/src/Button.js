2b737102ef654d7723ce9564c32520f0915409da
watching-dev <coderefactoring90@gmail.com>
2024-06-21 14:16:14 +0900

    [feat] PropTypes 설치 및 Button 컴포넌트에 적용_5 of 70_24.06.21

--- ReactJS/react-project/src/Button.js
+++ ReactJS/react-project/src/Button.js
@@ -1,4 +1,10 @@
+import PropTypes from "prop-types";
+
 function Button({ text }) {
   return <button>{text}</button>;
 }
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 export default Button;
