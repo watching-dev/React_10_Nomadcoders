0412cf750480947356a30a7fdd7bea7fc864b104
watching-dev <coderefactoring90@gmail.com>
2024-06-21 19:11:25 +0900

    [feat] 새로운 기능을 위해 다 지우고 state와 버튼 하나 생성_5 of 70_24.06.21

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -1,31 +1,12 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyWord, setKeyWord] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyWord(event.target.value);
-  const onlyOnce = () => {
-    console.log("only once");
-  };
-  useEffect(onlyOnce, []);
-  useEffect(() => {
-    if (keyWord !== "" && keyWord.length > 5) {
-      console.log("keyWord");
-    }
-  }, [keyWord]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
     <div>
-      <input
-        value={keyWord}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
     </div>
   );
 }
