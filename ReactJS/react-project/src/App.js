c79c2be855b39a3605b21248785eaed4c3ecdbef
watching-dev <coderefactoring90@gmail.com>
2024-06-22 23:25:39 +0900

    [feat] 챌린지를 위해 코드 정리_6 of 70_24.06.22

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -1,39 +1,7 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function destroyed() {
-    console.log("destroyed");
-  }
-  function created() {
-    console.log("created");
-    return destroyed;
-  }
-  useEffect(created, []);
-  // useEffect 만드는 법 비교
-  // useEffect(() => {
-  //   console.log("created");
-  //   return () => console.log("destroyed");
-  // }, []);
-
-  // useEffect(function () {
-  //   console.log("created");
-  //   return function () {
-  //     console.log("destroyed");
-  //   };
-  // }, []);
-  return <h1>hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
