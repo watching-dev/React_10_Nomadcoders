79b5d687aeb09ce83aba9c79c12a06368ddd24f3
watching-dev <coderefactoring90@gmail.com>
2024-06-25 00:57:01 +0900

    [feat] 이미지 출력 추가_7 of 70_24.06.25

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -24,6 +24,7 @@ function App() {
         <div>
           {movies.map((movie) => (
             <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
               <h2>{movie.title}</h2>
               <p>{movie.summary}</p>
               <ul>
