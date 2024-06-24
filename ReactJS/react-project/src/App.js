5672681010754a6b0fec906ed36ea7c8d3518804
watching-dev <coderefactoring90@gmail.com>
2024-06-24 17:25:22 +0900

    [feat] 다시 새로운 것을 하기위해 코드 정리_7 of 70_24.06.24

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -1,43 +1,7 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setTodo("");
-    setToDos((currentArray) => [toDo, ...currentArray]); // -- 이거랑
-    // toDo가 3이고 ...currentArray라면
-    // [[0], [1], [2]] 이것과 같음 그냥 currentArray로 적으면 전체 배열을 그대로 넣어버림
-    // [[Array]] ...이 없으면 이렇게 됨(초기값은 [] 니깐)
-    // setToDos(function(currentArray){ // -- 이거랑 같음
-    //   return []
-    // } )
-  };
-  return (
-    <div>
-      <h1>My Todo ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="write!"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
