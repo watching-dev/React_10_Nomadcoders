886b8e26bb70d6c92b6886c2af3fe16c528717c4
watching-dev <coderefactoring90@gmail.com>
2024-06-25 00:25:59 +0900

    [feat] api 통해서 데이터 가져오기_7 of 70_24.06.25

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -1,7 +1,15 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    )
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  });
+  return <div>{loading ? <h1>Loading!</h1> : null}</div>;
 }
 
 export default App;
