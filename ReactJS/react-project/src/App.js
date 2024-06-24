8a32e15c1e5a44dbbdf928f8be7c55f588b8d462
watching-dev <coderefactoring90@gmail.com>
2024-06-24 15:28:55 +0900

    [feat] onSubmit 내용 추가_7 of 70_24.06.24

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -3,7 +3,13 @@ import { useState, useEffect } from "react";
 function App() {
   const [toDo, setTodo] = useState("");
   const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => event.preventDefault();
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setTodo("");
+  };
   return (
     <div>
       <form onSubmit={{ onSubmit }}>
