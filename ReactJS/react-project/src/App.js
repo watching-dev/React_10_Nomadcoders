23d4017c2e8dd93dc17f0f208e0b7b4406e7825b
watching-dev <coderefactoring90@gmail.com>
2024-06-24 17:30:25 +0900

    [feat] state에 따라 로딩을 보여주도록 설정하고 fetch를 통해서 데이터를 가져옴_7 of 70_24.06.24

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -1,7 +1,16 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers");
+  }, []);
+  return (
+    <div>
+      <h1>The Conis!</h1>
+      {loading ? <strong>Loading!</strong> : null}
+    </div>
+  );
 }
 
 export default App;
