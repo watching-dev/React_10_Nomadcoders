1e7ec1cc1e2ce4aac36efe879677b3b0e0aaa8e1
watching-dev <coderefactoring90@gmail.com>
2024-06-24 15:26:40 +0900

    [feat] 버튼 추가하고 form 이벤트 추가_7 of 70_24.06.24

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -3,15 +3,17 @@ import { useState, useEffect } from "react";
 function App() {
   const [toDo, setTodo] = useState("");
   const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => event.preventDefault();
   return (
     <div>
-      <form>
+      <form onSubmit={{ onSubmit }}>
         <input
           value={toDo}
           onChange={onChange}
           type="text"
           placeholder="write!"
         />
+        <button>Add To Do</button>
       </form>
     </div>
   );
