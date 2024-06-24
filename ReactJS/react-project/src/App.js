a6beae1e946b8d5c6e80603ef69134320e1874e0
watching-dev <coderefactoring90@gmail.com>
2024-06-24 17:41:13 +0900

    [feat] 가져온 데이터를 coins에 입력_7 of 70_24.06.24

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -2,10 +2,14 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
   }, []);
   return (
     <div>
