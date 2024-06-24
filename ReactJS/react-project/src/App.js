0f859f188129161bcae8a70d49afed601e894743
watching-dev <coderefactoring90@gmail.com>
2024-06-25 00:33:39 +0900

    [feat] 데이터 파싱_7 of 70_24.06.25

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -2,13 +2,17 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     fetch(
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
     )
       .then((response) => response.json())
-      .then((json) => console.log(json));
-  });
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
   return <div>{loading ? <h1>Loading!</h1> : null}</div>;
 }
 
