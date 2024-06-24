d309089221111a880fc89fb64bd3649c6d05f52c
watching-dev <coderefactoring90@gmail.com>
2024-06-25 03:05:54 +0900

    [feat] prop을 통해 id 값을 가져온 것으로 다이나믹 url 적용_7 of 70_24.06.25

--- ReactJS/react-project/src/components/Movie.js
+++ ReactJS/react-project/src/components/Movie.js
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ medium_cover_image, title, summary, genres }) {
+function Movie({ medium_cover_image, title, summary, genres, id }) {
   return (
     <div>
       <img src={medium_cover_image} alt={title} />
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -20,6 +20,7 @@ function Movie({ medium_cover_image, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   medium_cover_image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
