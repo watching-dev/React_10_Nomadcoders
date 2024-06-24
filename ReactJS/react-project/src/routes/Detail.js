f276742756f0570e663ea66f12d1a9566ff580ea
watching-dev <coderefactoring90@gmail.com>
2024-06-25 03:20:44 +0900

    [feat] fetch해서 데이터 가져옴_7 of 70_24.06.25

--- ReactJS/react-project/src/routes/Detail.js
+++ ReactJS/react-project/src/routes/Detail.js
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const id = useParams();
   console.log(id);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return <h1>Detail</h1>;
 }
 
