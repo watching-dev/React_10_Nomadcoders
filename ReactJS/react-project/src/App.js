73b4c769d421544bd15612b0a194e8154cf0ea1f
watching-dev <coderefactoring90@gmail.com>
2024-06-25 00:21:18 +0900

    [feat] 새로운 프로젝트를 위해 코드 정리_7 of 70_24.06.25

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -1,32 +1,7 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The Conis! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading!</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
