a08ab81104672e99294f9ea6df84b43e38bf4288
watching-dev <coderefactoring90@gmail.com>
2024-06-25 00:38:19 +0900

    [feat] then -> async await으로 변경_7 of 70_24.06.25

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -3,15 +3,16 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
+  const getMovies = async () => {
+    const response = await fetch(
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
   }, []);
   return <div>{loading ? <h1>Loading!</h1> : null}</div>;
 }
