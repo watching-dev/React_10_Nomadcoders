05e14865e79d81c0e9eba82e0755cb06dcae069a
watching-dev <coderefactoring90@gmail.com>
2024-06-25 02:48:27 +0900

    [feat] Detail 라우터 추가_7 of 70_24.06.25

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/movie" element={<Detail />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
