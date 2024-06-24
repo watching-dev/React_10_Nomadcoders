06d7e929a14ddf0f2bc39114b6e175947f251a39
watching-dev <coderefactoring90@gmail.com>
2024-06-25 02:59:57 +0900

    [feat] 동적 url(변수) 이용 추가(:id)_7 of 70_24.06.25

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
