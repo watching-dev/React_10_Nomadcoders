f4b715b663671435e74e17e28c399c4cdf8103ef
watching-dev <coderefactoring90@gmail.com>
2024-06-25 00:40:34 +0900

    [feat] async await 축약_7 of 70_24.06.25

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -4,10 +4,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    );
-    const json = await response.json();
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
