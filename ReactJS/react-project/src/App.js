c9647c1f5740896f48a0a4d8b0f34bc23a89dad2
watching-dev <coderefactoring90@gmail.com>
2024-06-24 17:35:01 +0900

    [feat] api를 통해서 fetch로 데이터를 json 형태로 가져옴_7 of 70_24.06.24

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -3,7 +3,9 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers");
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
   }, []);
   return (
     <div>
