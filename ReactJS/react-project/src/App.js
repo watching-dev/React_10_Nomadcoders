f011d5e5356d6b1f14648994b0464aa5b9497b4d
watching-dev <coderefactoring90@gmail.com>
2024-06-25 14:06:30 +0900

    [feat] 깃허브 페이지 path 수정_7 of 70_24.06.25

--- ReactJS/react-project/src/App.js
+++ ReactJS/react-project/src/App.js
@@ -6,8 +6,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path="/" element={<Home />}></Route>
+        {/* <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route> */}
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        ></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
       </Routes>
     </Router>
   );
