d4656f328ae9c3a7c58a22adf253925dbdbed762
watching-dev <coderefactoring90@gmail.com>
2024-07-01 17:06:41 +0900

    [feat] App.js 수정_15 of 70_24.07.01

--- React-Master/react-master/src/App.js
+++ React-Master/react-master/src/App.js
@@ -1,5 +1,12 @@
+import styled from "styled-components";
+
 function App() {
-  return null;
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
+      <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
+    </div>
+  );
 }
 
 export default App;
