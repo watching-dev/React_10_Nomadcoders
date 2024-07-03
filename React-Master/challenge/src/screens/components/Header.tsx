2ed7b6e9e6e0515a12391c0700132e0aba2368a7
watching-dev <coderefactoring90@gmail.com>
2024-07-04 00:55:35 +0900

    [challenge] useNavigate

--- React-Master/challenge/src/screens/components/Header.tsx
+++ React-Master/challenge/src/screens/components/Header.tsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function Header() {
+  const navigate = useNavigate();
+  const onAboutClick = () => {
+    navigate("/about");
+  };
   return (
     <ul>
       <li>
         <Link to={"/"}>Home</Link>
       </li>
       <li>
-        <Link to={"/about"}>About</Link>
+        <button onClick={onAboutClick}>About</button>
       </li>
     </ul>
   );
