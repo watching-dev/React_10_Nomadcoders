9e4952dddba83028647f20dd6513ea15fc82cf39
watching-dev <coderefactoring90@gmail.com>
2024-07-05 20:34:50 +0900

    [add] 구글 폰트 https://fonts.google.com 및_  Source Sans Pro 폰트 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap'); font-family: 'Source Sans Pro', sans-serif; 및_ Flat UI Color https://flatuicolors.com/palette/gb 및 createGlobalStyle (전역 스타일을 처리함) 및 _ 전역 스타일을 처리하는 특수 Styled Component를 생성하는 helper 함수입니다. https://styled-components.com/docs/api\#createglobalstyle 추가

--- React-Master/react-master-challenge/src/App.tsx
+++ React-Master/react-master-challenge/src/App.tsx
@@ -2,6 +2,8 @@ import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+font-family: 'Source Sans Pro', sans-serif;
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -48,6 +50,18 @@ q:before, q:after {
 table {
   border-collapse: collapse;
   border-spacing: 0;
+}
+
+*{
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Source Sans Pro', sans-serif;
+  background-color: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.textColor};
+}
+a {
+  text-decoration: none;
 }
 `;
 
---
+++ React-Master/react-master-challenge/src/styled.d.ts
@@ -0,0 +1,9 @@
+import "styled-components";
+
+declare module "style-components" {
+  export interface DefaultTheme {
+    textColor: string;
+    bgColor: string;
+    accentColor: string;
+  }
+}
