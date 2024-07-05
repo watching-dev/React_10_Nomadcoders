4d96d0b241d5cc855eee101c78828593f32bbc76
watching-dev <coderefactoring90@gmail.com>
2024-07-06 01:27:28 +0900

    [fix] react-dom.development.js:86 Warning: React does not recognize the  prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase  instead. If you accidentally passed it from a parent component, remove it from the DOM element. React18 이후 일관성을 높이고, 사용자 혼동을 방지하기 위해 prop의 이름은 소문자나 앞에 $가 있어야만 사용자 지정 속성으로 인식함. $가 추가된 이유는 일부 라이브러리 또는 구성 요소는 대문자를 사용하는 prop을 요구하기 때문. - 에러 수정

--- React-Master/react-master-challenge/src/routes/Coin.tsx
+++ React-Master/react-master-challenge/src/routes/Coin.tsx
@@ -63,7 +63,7 @@ const Tabs = styled.div`
   gap: 10px;
 `;
 
-const Tab = styled.span<{ isActive: boolean }>`
+const Tab = styled.span<{ $isActive: boolean }>`
   text-align: center;
   text-transform: uppercase;
   font-size: 12px;
@@ -72,7 +72,7 @@ const Tab = styled.span<{ isActive: boolean }>`
   padding: 7px 0px;
   border-radius: 10px;
   color: ${(props) =>
-    props.isActive ? props.theme.accentColor : props.theme.textColor};
+    props.$isActive ? props.theme.accentColor : props.theme.textColor};
   a {
     display: block;
   }
@@ -201,10 +201,10 @@ function Coin() {
             </OverviewItem>
           </Overview>
           <Tabs>
-            <Tab isActive={chartMatch !== null}>
+            <Tab $isActive={chartMatch !== null}>
               <Link to={`/${coinId}/chart`}>Chart</Link>
             </Tab>
-            <Tab isActive={priceMatch !== null}>
+            <Tab $isActive={priceMatch !== null}>
               <Link to={`/${coinId}/price`}>Price</Link>
             </Tab>
           </Tabs>
