5ae5b3e79021828b2f253df6dc9eccdae4dcb869
watching-dev <coderefactoring90@gmail.com>
2024-07-08 23:23:50 +0900

    [challenge] todo list 추가

--- React-Master/react-master-challenge/src/App.tsx
+++ React-Master/react-master-challenge/src/App.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -71,6 +72,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
---
+++ React-Master/react-master-challenge/src/ToDoList.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function ToDoList() {
+  const [toDo, setTodo] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setTodo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default ToDoList;
