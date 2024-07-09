7439b1733a5a481bd0cb70ce61ebacd5c16886b3
watching-dev <coderefactoring90@gmail.com>
2024-07-09 15:08:34 +0900

    [challenge] 챌린지 구현

--- React-Master/react-master-challenge/src/ToDoList.tsx
+++ React-Master/react-master-challenge/src/ToDoList.tsx
@@ -1,117 +1,182 @@
 import { error } from "console";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { atom, selector, useRecoilValue, useSetRecoilState } from "recoil";
+import styled from "styled-components";
 
-// function ToDoList() {
-//   const [toDo, setTodo] = useState("");
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setTodo(value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
-//         <button>Add</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-interface IForm {
-  todo1: string;
-  todo2: string;
-  todo3: string;
-  todo4: string;
-  todo5: string;
-  todo6: string;
-  extraError?: string;
+export enum SelectKeys {
+  "GONE" = "GONE",
+  "LIKE" = "LIKE",
+  "DEL" = "DEL",
+  "WANT" = "WANT",
 }
 
-function ToDoList() {
+export interface IToDo {
+  id: number;
+  text: string;
+  filter: SelectKeys;
+}
+
+export const todoFilterState = atom<SelectKeys>({
+  key: "todoFilterState",
+  default: SelectKeys.WANT,
+});
+
+export const todoState = atom<IToDo[]>({
+  key: "todoState",
+  default: JSON.parse(localStorage.getItem("list") || "[]"),
+});
+
+export const toDoSelector = selector({
+  key: "todoSelector",
+  get: ({ get }) => {
+    const todos = get(todoState);
+
+    return [
+      todos.filter((toDo) => toDo.filter === SelectKeys.GONE),
+      todos.filter((toDo) => toDo.filter === SelectKeys.LIKE),
+      todos.filter((toDo) => toDo.filter === SelectKeys.WANT),
+    ];
+  },
+});
+
+const ToDo = ({ text, filter, id }: IToDo) => {
+  const setList = useSetRecoilState(todoState);
+  const onClick = (setFilterKey: IToDo["filter"]) => {
+    setList((prevToDos) => {
+      console.log(prevToDos.map((tod) => console.log(tod)));
+      if (setFilterKey === SelectKeys.DEL) {
+        return prevToDos.filter((toDo) => toDo.id !== id);
+      }
+
+      return prevToDos.map((toDo) =>
+        toDo.id === id ? { ...toDo, filter: setFilterKey } : toDo
+      );
+    });
+  };
+
+  return (
+    <div>
+      <span>{text}</span>
+      {filter === SelectKeys.WANT && (
+        <>
+          <button onClick={() => onClick(SelectKeys.GONE)}>✅</button>
+          <button onClick={() => onClick(SelectKeys.DEL)}>🗑️</button>
+        </>
+      )}
+      {filter === SelectKeys.GONE && (
+        <>
+          <button onClick={() => onClick(SelectKeys.LIKE)}>👍</button>
+          <button onClick={() => onClick(SelectKeys.WANT)}>❌</button>
+        </>
+      )}
+      {filter === SelectKeys.LIKE && (
+        <>
+          <button onClick={() => onClick(SelectKeys.GONE)}>👎</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+const CreateToDo = () => {
+  const setToDoList = useSetRecoilState(todoState);
+  const filterKey = useRecoilValue(todoFilterState);
+
   const {
     register,
     watch,
     handleSubmit,
     formState: { errors },
     setError,
+    setValue,
   } = useForm<IForm>({
-    defaultValues: {
-      todo1: "@naver.com",
-    },
+    defaultValues: {},
   });
-  const onValid = (data: IForm) => {
-    setError(
-      "extraError",
-      {
-        message: "Server offline",
-      },
-      {
-        shouldFocus: true,
-      }
-    );
+
+  const handleValid = ({ name }: IForm) => {
+    setToDoList((prevToDos) => [
+      ...prevToDos,
+      { text: name, id: Date.now(), filter: filterKey },
+    ]);
+    setValue("name", "");
   };
+
   return (
-    <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
-        <input
-          {...register("todo1", {
-            required: "require",
-            validate: (value) => (!value.includes("nico") ? "no allow" : true),
-            pattern: /^[A-Za-z0-9._%+-]+@naver.com$/,
-          })}
-          placeholder="Write a to do1"
-        />
-        <span>{errors?.todo1?.message as string}</span>
-        <input
-          {...register("todo2", { required: "require", minLength: 10 })}
-          placeholder="Write a to do2"
-        />
-        <span>{errors?.todo2?.message as string}</span>
-        <input
-          {...register("todo3", {
-            required: "email require",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "only naver",
-            },
-          })}
-          placeholder="Write a to do3"
-        />
-        <span>{errors?.todo3?.message as string}</span>
-        <input
-          {...register("todo4", { required: "require" })}
-          placeholder="Write a to do4"
-        />
-        <span>{errors?.todo4?.message as string}</span>
-        <input
-          {...register("todo5", { required: "require" })}
-          placeholder="Write a to do5"
-        />
-        <span>{errors?.todo5?.message as string}</span>
-        <input
-          {...register("todo6", {
-            required: "password is required",
-            minLength: {
-              value: 5,
-              message: "short",
-            },
-          })}
-          placeholder="Write a to do6"
-        />
-        <span>{errors?.todo6?.message as string}</span>
-        <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
-      </form>
-    </div>
+    <form
+      style={{ display: "flex", flexDirection: "column", color: "white" }}
+      onSubmit={handleSubmit(handleValid)}
+    >
+      <input
+        {...register("name", {
+          required: "required!!",
+        })}
+        placeholder="이름"
+      />
+      <span>{errors?.name?.message as string}</span>
+      <button>가자!</button>
+    </form>
+  );
+};
+
+const Container = styled.div`
+  padding: 10px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  color: white;
+`;
+
+interface IForm {
+  name: string;
+  extraError?: string;
+}
+
+function ToDoList() {
+  const list = useRecoilValue(todoState);
+  const [gone, like, want] = useRecoilValue(toDoSelector);
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <Container>
+      <Header>
+        <Title>내가 가고싶은 나라들</Title>
+      </Header>
+      <CreateToDo />
+      <div>
+        <h2></h2>
+        <div>
+          {want.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+          ))}
+        </div>
+        <h2>내가 가본 나라들</h2>
+        <div>
+          {gone.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+          ))}
+        </div>
+        <h2>내가 좋아하는 나라들</h2>
+        <div>
+          {like.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+          ))}
+        </div>
+      </div>
+    </Container>
   );
 }
 
--- React-Master/react-master-challenge/src/theme.ts
+++ React-Master/react-master-challenge/src/theme.ts
@@ -2,6 +2,6 @@ import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
   bgColor: "#2f3640",
-  textColor: "black",
+  textColor: "white",
   accentColor: "#4cd137",
 };
