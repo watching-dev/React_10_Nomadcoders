aa5c07ca4a4c3a057f11d859df341a5cfe7dc31c
watching-dev <coderefactoring90@gmail.com>
2024-07-12 16:42:23 +0900

    [feat] useRef 사용, 특정 범위안에서만 드래그 되도록

--- React-Master/react-master/src/App.js
+++ React-Master/react-master/src/App.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { delay, motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -13,26 +14,42 @@ const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 10px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled(motion.div)`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const boxVariant = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
+  hover: { srotateZ: 90 },
+  click: { borderRadius: "100px" },
 };
 
 function App() {
+  // const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const biggerBoxRef = useRef(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariant}
-        whileHover="hover"
-        whileTap="click"
-        whileDrag="drag"
-      ></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          // dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariant}
+          whileHover="hover"
+          whileTap="click"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
