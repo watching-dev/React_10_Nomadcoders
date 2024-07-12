75b9cad0c40c0d8d28121f3aaa7d9e82052be29f
watching-dev <coderefactoring90@gmail.com>
2024-07-12 14:28:53 +0900

    [feat] variants_애니메이션 기존 설정을 object로 분리

--- React-Master/react-master/src/App.js
+++ React-Master/react-master/src/App.js
@@ -17,18 +17,15 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", damping: 300 } },
+};
+
 function App() {
   return (
     <Wrapper>
-      {/* <Box transition={{ delay: 3 }} animate={{ borderRadius: "100px" }} />
-      <Box transition={{ duration: 3 }} animate={{ borderRadius: "100px" }} /> */}
-      <Box
-        // transition={{ type: "tween" }}
-        // transition={{ type: "spring", stiffness: 10 }}
-        transition={{ type: "spring", damping: 300 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
