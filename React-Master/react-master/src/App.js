9c12ccdf9a3bf083d319f602bb47eb838728fe45
watching-dev <coderefactoring90@gmail.com>
2024-07-12 16:24:38 +0900

    [feat] drag 추가

--- React-Master/react-master/src/App.js
+++ React-Master/react-master/src/App.js
@@ -12,53 +12,27 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 10px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  background-color: white;
-  border-radius: 40px;
-  place-self: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariant = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const circleVariants = {
-  // start: { scale: 0 },
-  // end: { scale: 2, transition: { type: "spring", bounce: 0.8, duration: 5 } },
-  start: { opacity: 0, y: 10 },
-  end: { opacity: 1, y: 0 /*transition: { delay: 0.5 }*/ },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariant} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box
+        drag
+        variants={boxVariant}
+        whileHover="hover"
+        whileTap="click"
+        whileDrag="drag"
+      ></Box>
     </Wrapper>
   );
 }
