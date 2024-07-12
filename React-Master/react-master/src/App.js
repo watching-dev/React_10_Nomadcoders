08b422855a9c3a3e60a5b90470fbb782724635ac
watching-dev <coderefactoring90@gmail.com>
2024-07-12 18:40:12 +0900

    [feat] scroll_motionValue는 state가 아니라서 리랜더링을 안함

--- React-Master/react-master/src/App.js
+++ React-Master/react-master/src/App.js
@@ -4,12 +4,13 @@ import {
   motion,
   useMotionValue,
   useMotionValueEvent,
+  useScroll,
   useTransform,
 } from "framer-motion";
 import { useEffect, useRef } from "react";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -29,13 +30,29 @@ function App() {
   // useEffect(() => {
   //   x.on("change", () => console.log(x.get()));
   // }, [x]);
-  const potato = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
-  useMotionValueEvent(potato, "change", (latest) => {
+  const rotateZ = useTransform(x, [-800, 800], [360, -360]);
+  useMotionValueEvent(rotateZ, "change", (latest) => {
     console.log("x changed to", latest);
   });
-
+  const gradient = useTransform(
+    x,
+    [-800, /*0,*/ 800],
+    [
+      "linear-gradient(135deg, rgb(0,210,238),rgb(0,83,238)",
+      // "linear-gradient(135deg, rgb(238,0,153),rgb(221,0,238)",
+      "linear-gradient(135deg, rgb(0,238,155),rgb(238,178,0)",
+    ]
+  );
+  // motionValue는 state가 아니라서 리랜더링을 안함. 차이점 주의
+  const { scrollY, scrollYProgress } = useScroll();
+  useEffect(() => {
+    scrollY.on("change", () =>
+      console.log(scrollY.get(), scrollYProgress.get())
+    );
+  }, [scrollY, scrollYProgress]);
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
+    <Wrapper style={{ background: gradient }}>
       <button
         onClick={() => {
           x.set(200);
@@ -43,7 +60,7 @@ function App() {
       >
         click me
       </button>
-      <Box style={{ x, scale: potato }} drag="x" dragSnapToOrigin></Box>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin></Box>
     </Wrapper>
   );
 }
