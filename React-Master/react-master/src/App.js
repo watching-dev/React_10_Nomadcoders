c9b7e6135fa7b94c9e5cd456e40ff4dc7ca7be0e
watching-dev <coderefactoring90@gmail.com>
2024-07-12 14:55:05 +0900

    [feat] dealyChildren, staggerChildren 추가

--- React-Master/react-master/src/App.js
+++ React-Master/react-master/src/App.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { delay, motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -12,20 +12,53 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", damping: 300 } },
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  background-color: white;
+  border-radius: 40px;
+  place-self: center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariant = {
+  start: { opacity: 0, scale: 0.5 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  // start: { scale: 0 },
+  // end: { scale: 2, transition: { type: "spring", bounce: 0.8, duration: 5 } },
+  start: { opacity: 0, y: 10 },
+  end: { opacity: 1, y: 0 /*transition: { delay: 0.5 }*/ },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVariant} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
