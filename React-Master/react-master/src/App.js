3b23698981d38375807abd3a135d97ebb91eb483
watching-dev <coderefactoring90@gmail.com>
2024-07-12 18:06:39 +0900

    [feat] useMotionValue 리액트는 매번 모든 컴포넌트를 리랜더링 하지 않음. 그래서 스크롤할 때 값을 가져오려면 useEffect나 useMotionValueEvent를 이용해서 가져올 수 있음. 이렇게 안하면 console.log는 한번밖에 출력 안함

--- React-Master/react-master/src/App.js
+++ React-Master/react-master/src/App.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
-import { delay, motion } from "framer-motion";
-import { useRef } from "react";
+import {
+  delay,
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+} from "framer-motion";
+import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -18,38 +23,25 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const BiggerBox = styled(motion.div)`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-`;
-
-const boxVariant = {
-  hover: { srotateZ: 90 },
-  click: { borderRadius: "100px" },
-};
-
 function App() {
-  // const biggerBoxRef = useRef<HTMLDivElement>(null);
-  const biggerBoxRef = useRef(null);
+  const x = useMotionValue(0);
+  // useEffect(() => {
+  //   x.on("change", () => console.log(x.get()));
+  // }, [x]);
+  useMotionValueEvent(x, "change", (latest) => {
+    console.log("x changed to", latest);
+  });
+
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          // dragSnapToOrigin
-          dragElastic={0.5}
-          dragConstraints={biggerBoxRef}
-          variants={boxVariant}
-          whileHover="hover"
-          whileTap="click"
-        ></Box>
-      </BiggerBox>
+      <button
+        onClick={() => {
+          x.set(200);
+        }}
+      >
+        click me
+      </button>
+      <Box style={{ x }} drag="x" dragSnapToOrigin></Box>
     </Wrapper>
   );
 }
