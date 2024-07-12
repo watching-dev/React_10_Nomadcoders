5c2ac993b3d691d028421fe7c27dd772ad56bbd1
watching-dev <coderefactoring90@gmail.com>
2024-07-12 21:03:43 +0900

    [feat] 서로 다른 컴포넌트를 똑같은 컴포넌트로 묶으면(layoutId 를 같은걸로 적으면) 애니메이션을 자동으로 해줌_layoutId가 없으면 그냥 reactJs가 해주는걸로 보임_그걸 자동으로 애니메이션을 만들어줌

--- React-Master/react-master/src/App.tsx
+++ React-Master/react-master/src/App.tsx
@@ -24,10 +24,12 @@ const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
-  position: absolute;
+  /* position: absolute; */
   top: 10px;
   border-radius: 40px;
   display: flex;
+  justify-content: center;
+  align-items: center;
   font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
@@ -36,7 +38,7 @@ const Circle = styled(motion.div)`
   background-color: #00a5ff;
   height: 100px;
   width: 100px;
-  border-radius: 50px;
+  /* border-radius: 50px; */
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -45,13 +47,15 @@ function App() {
   const toggleClicked = () => setClicked((prev) => !prev);
   return (
     <Wrapper onClick={toggleClicked}>
-      <Box
-        style={{
-          justifyContent: clicked ? "center" : "flex-start",
-          alignItems: clicked ? "center" : "flex-start",
-        }}
-      >
-        <Circle layout />
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+        ) : null}
       </Box>
     </Wrapper>
   );
