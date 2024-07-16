9596bbc419078e23e5f32e30952711d9e8867a11
watching-dev <coderefactoring90@gmail.com>
2024-07-16 18:14:44 +0900

    [feat] Header 수정_30 of 70_24.07.16

--- React-Master/react-master/src/Components/Header.tsx
+++ React-Master/react-master/src/Components/Header.tsx
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Col = styled.div``;
+
+const Logo = styled.svg`
+  margin-right: 50px;
+`;
+
+const Items = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const Item = styled.li`
+  margin-right: 20px;
+`;
+
 function Header() {
-  return null;
+  return (
+    <Nav>
+      <Col>
+        <Logo />
+        <Items>
+          <Item>Home</Item>
+          <Item>TvShow</Item>
+        </Items>
+      </Col>
+      <Col>
+        <button>search</button>
+      </Col>
+    </Nav>
+  );
 }
 
 export default Header;
