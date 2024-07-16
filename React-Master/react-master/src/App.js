ca1862a073ff8653c81bef94ee9be12b62fe3435
watching-dev <coderefactoring90@gmail.com>
2024-07-16 17:50:48 +0900

    [feat] Header 추가_30 of 70_24.07.16

--- React-Master/react-master/src/App.js
+++ React-Master/react-master/src/App.js
@@ -1,12 +1,23 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
-        <Route path="/"></Route>
-        <Route path="/tv"></Route>
-        <Route path="/search"></Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
       </Switch>
     </Router>
   );
