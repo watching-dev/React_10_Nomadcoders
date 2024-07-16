44e838b1fd01289581024f549b3275fe697a5350
watching-dev <coderefactoring90@gmail.com>
2024-07-16 17:47:22 +0900

    [feat] Route 설정_30 of 70_24.07.16

--- React-Master/react-master/src/App.js
+++ React-Master/react-master/src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/"></Route>
+        <Route path="/tv"></Route>
+        <Route path="/search"></Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
