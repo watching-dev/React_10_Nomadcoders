ca0ae62ad0bde57f964e94793251cc9608718d71
watching-dev <coderefactoring90@gmail.com>
2024-07-17 20:09:36 +0900

    [feat] loader 추가하고 전체 body css 변경_31 of 70_24.07.17

--- React-Master/react-master/src/Routes/Home.tsx
+++ React-Master/react-master/src/Routes/Home.tsx
@@ -1,13 +1,26 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import { getMovies, IGetMoviesResult } from "../api";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function Home() {
-  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
   console.log(data, isLoading);
   console.log("----");
-  return (
-    <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>
-  );
+  return <Wrapper>{isLoading ? <Loader>loading</Loader> : null}</Wrapper>;
 }
 
 export default Home;
