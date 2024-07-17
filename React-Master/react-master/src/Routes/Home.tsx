3e0f6fc51536b58ec3099e240723f8056c0d1464
watching-dev <coderefactoring90@gmail.com>
2024-07-17 20:30:28 +0900

    [feat] Banner 추가_31 of 70_24.07.17

--- React-Master/react-master/src/Routes/Home.tsx
+++ React-Master/react-master/src/Routes/Home.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import { getMovies, IGetMoviesResult } from "../api";
 import styled from "styled-components";
+import { makeImagePath } from "../utils";
 
 const Wrapper = styled.div`
   background-color: black;
@@ -13,6 +14,27 @@ const Loader = styled.div`
   align-items: center;
 `;
 
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+`;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+  margin-bottom: 20px;
+`;
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
@@ -20,7 +42,20 @@ function Home() {
   );
   console.log(data, isLoading);
   console.log("----");
-  return <Wrapper>{isLoading ? <Loader>loading</Loader> : null}</Wrapper>;
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>loading</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
