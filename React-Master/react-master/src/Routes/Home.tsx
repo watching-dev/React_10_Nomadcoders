bf2f7166db6049cd8a3380bedb5619e8d661165b
watching-dev <coderefactoring90@gmail.com>
2024-07-18 15:53:30 +0900

    [feat] 새로고침 하거나 다른 페이지 갔다가 돌아오면 오른쪽에서 애니메이션으로 이동하는 문제 수정_32 of 70_24.07.1

--- React-Master/react-master/src/Routes/Home.tsx
+++ React-Master/react-master/src/Routes/Home.tsx
@@ -95,7 +95,7 @@ function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence onExitComplete={toggleLeaving}>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVariants}
                 initial="hidden"
