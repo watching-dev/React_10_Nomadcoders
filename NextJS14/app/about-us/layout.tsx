5593c5110240ca7d225aadddc84a68a652c7fde2
watching-dev <coderefactoring90@gmail.com>
2024-07-23 20:20:40 +0900

    [feat] about-us layout에 metadata를 수정하고 이 페이지로 이동하면 여기에 작성한 타이틀로 변경, description은 그대로

--- NextJS14/app/about-us/layout.tsx
+++ NextJS14/app/about-us/layout.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About | Next!!",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <div>{children} &copy; NextJS!</div>;
 }
