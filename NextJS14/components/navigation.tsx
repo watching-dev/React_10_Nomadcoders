4936b6b6b034532ee73e2d3a082c0f8339d77885
watching-dev <coderefactoring90@gmail.com>
2024-07-23 14:32:10 +0900

    [feat] 현재 페이지 위치를 알려주는 navigation hook : usePathname 추가

--- NextJS14/components/navigation.tsx
+++ NextJS14/components/navigation.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+  const path = usePathname();
   return (
     <nav>
       <ul>
