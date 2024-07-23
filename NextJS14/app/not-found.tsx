29ae46f62051c218470b480e764eae5e293e187b
watching-dev <coderefactoring90@gmail.com>
2024-07-23 20:29:06 +0900

    [feat] not found에 metadata 추가

--- NextJS14/app/not-found.tsx
+++ NextJS14/app/not-found.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import Navigation from "../components/navigation";
+
+export const metadata: Metadata = {
+  title: "Not Found",
+};
 
 export default function NotFound() {
   return (
