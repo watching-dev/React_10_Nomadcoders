77132c33a286db7fda0d547aafe0fb429f1cefd7
watching-dev <coderefactoring90@gmail.com>
2024-07-23 15:22:05 +0900

    [feat] 기존 복붙한 navigation을 layout에 한번 사용으로 변경

--- NextJS14/app/about-us/page.tsx
+++ NextJS14/app/about-us/page.tsx
@@ -3,7 +3,6 @@ import Navigation from "../../components/navigation";
 export default function AboutUs() {
   return (
     <div>
-      <Navigation />
       <h1>about</h1>
     </div>
   );
--- NextJS14/app/page.tsx
+++ NextJS14/app/page.tsx
@@ -3,7 +3,6 @@ import Navigation from "../components/navigation";
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>hello</h1>
     </div>
   );
