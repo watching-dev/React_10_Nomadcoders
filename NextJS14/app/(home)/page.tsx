cad764e806764b27320cce0b6480fb21151ca5ae
watching-dev <coderefactoring90@gmail.com>
2024-07-24 21:41:10 +0900

    [feat] 클라이언트가 아닌 넥스트 백엔드에서 server components에서 데이터 fetching으로 수정, fetch를 하면 캐싱을 사용해서 로딩이 없음

--- NextJS14/app/(home)/page.tsx
+++ NextJS14/app/(home)/page.tsx
@@ -1,21 +1,17 @@
-"use client";
+export const metadata = {
+  title: "Home",
+};
 
-import { useEffect, useState } from "react";
-import Navigation from "../../components/navigation";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-  const [isLoaidng, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return <div>{isLoaidng ? "Loading.." : JSON.stringify(movies)}</div>;
+async function getMovies() {
+  // return fetch(URL).then((response) => response.json());
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
