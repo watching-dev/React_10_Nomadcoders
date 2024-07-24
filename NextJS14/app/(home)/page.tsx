0cbb5b02a0418d5c159fb6890d6524370ba9b596
watching-dev <coderefactoring90@gmail.com>
2024-07-24 21:26:48 +0900

    [feat] react에서 data fetching 하는 방법

--- NextJS14/app/(home)/page.tsx
+++ NextJS14/app/(home)/page.tsx
@@ -4,16 +4,18 @@ import { useEffect, useState } from "react";
 import Navigation from "../../components/navigation";
 
 export default function Page() {
-  const [movies, setMovies] = useState();
+  const [isLoaidng, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
   const getMovies = async () => {
     const response = await fetch(
       "https://nomad-movies.nomadcoders.workers.dev/movies"
     );
     const json = await response.json();
     setMovies(json);
+    setIsLoading(false);
   };
   useEffect(() => {
     getMovies();
   }, []);
-  return <div>{JSON.stringify(movies)}</div>;
+  return <div>{isLoaidng ? "Loading.." : JSON.stringify(movies)}</div>;
 }
