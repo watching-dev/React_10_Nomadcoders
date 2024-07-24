ba6890c7fafa03fa7587d8f106b48c5c53491036
watching-dev <coderefactoring90@gmail.com>
2024-07-24 21:18:35 +0900

    [feat] movie data 가져오기 추가

--- NextJS14/app/(home)/page.tsx
+++ NextJS14/app/(home)/page.tsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from "react";
 import Navigation from "../../components/navigation";
+import { json } from "stream/consumers";
 
 export const metadata = {
   title: "Home",
 };
 
 export default function Page() {
+  const [movies, setMovies] = useState();
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/moives"
+    );
+    const json = await response.json();
+    setMovies(json);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
       <h1>hello</h1>
+      {JSON.stringify(json)}
     </div>
   );
 }
