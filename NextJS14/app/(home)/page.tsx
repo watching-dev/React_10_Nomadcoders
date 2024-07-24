e06991890c3be2cdfe5d7c2a1a805a3c73b2bba4
watching-dev <coderefactoring90@gmail.com>
2024-07-25 00:01:54 +0900

    [feat] movie 데이터 가져오기 수정

--- NextJS14/app/(home)/page.tsx
+++ NextJS14/app/(home)/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Home",
 };
@@ -5,7 +7,7 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   // return fetch(URL).then((response) => response.json());
   const response = await fetch(URL);
   const json = await response.json();
@@ -14,5 +16,13 @@ async function getMovies() {
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
