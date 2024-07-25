2363416d2154097f0214be84a041f8f2d3c5055d
watching-dev <coderefactoring90@gmail.com>
2024-07-25 11:12:36 +0900

    [feat] home셜정

--- NextJS14/app/(home)/page.tsx
+++ NextJS14/app/(home)/page.tsx
@@ -19,9 +19,10 @@ export default async function HomePage() {
   return (
     <div>
       {movies.map((movie) => (
-        <li key={movie.id}>
+        <div key={movie.id}>
+          <img src={movie.poster_path} alt={movie.title} />
           <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        </div>
       ))}
     </div>
   );
