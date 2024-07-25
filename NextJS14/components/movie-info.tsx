84bfe48d8bcd88d4cedc4bff4f1d880662b63b74
watching-dev <coderefactoring90@gmail.com>
2024-07-25 11:59:09 +0900

    [feat] movie-info.module.css 설정

--- NextJS14/components/movie-info.tsx
+++ NextJS14/components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
 async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
@@ -8,12 +9,19 @@ async function getMovie(id: string) {
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
   return (
-    <div>
-      <img src={movie.poster_path} />
-      <div>
-        <h1>{movie.title}</h1>
-        <h3>{movie.vote_average}</h3>
+    <div className={styles.container}>
+      <img
+        src={movie.poster_path}
+        className={styles.poster}
+        alt={movie.title}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>{movie.vote_average.toFixed(1)}</h3>
         <p>{movie.overview}</p>
+        <a href={movie.hompage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
       </div>
     </div>
   );
