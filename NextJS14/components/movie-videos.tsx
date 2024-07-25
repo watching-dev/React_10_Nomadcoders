75c89d33d1af9d3f9741c93c6cc90870e4017f57
watching-dev <coderefactoring90@gmail.com>
2024-07-25 11:37:59 +0900

    [feat] 강제 에러 발생 삭제

--- NextJS14/components/movie-videos.tsx
+++ NextJS14/components/movie-videos.tsx
@@ -1,9 +1,9 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-  throw new Error("error!");
-  //   const response = await fetch(`${API_URL}/${id}/videos`);
-  //   return response.json();
+  // throw new Error("error!");
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
