e6ec975db1cb8eb5b5cf86fd570d599b0d363ea7
watching-dev <coderefactoring90@gmail.com>
2024-07-25 11:34:38 +0900

    [feat] 그림에 클릭 이벤트 설정

--- NextJS14/components/movie.tsx
+++ NextJS14/components/movie.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from "../styles/movie.module.css";
+import { useRouter } from "next/router";
 
 interface IMovieProps {
   id: string;
@@ -8,9 +9,13 @@ interface IMovieProps {
 }
 
 export default function Movie({ id, title, poster_path }: IMovieProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/movies/${id}`);
+  };
   return (
     <div className={styles.movie}>
-      <img src={poster_path} alt={title} />
+      <img src={poster_path} alt={title} onClick={onClick} />
       <Link href={`/movies/${id}`}>{title}</Link>
     </div>
   );
