d5341c7dc21e7965c1380d4bb8758b660061f0b4
watching-dev <coderefactoring90@gmail.com>
2024-07-25 12:04:45 +0900

    [feat] movie videos 설정

--- NextJS14/components/movie-videos.tsx
+++ NextJS14/components/movie-videos.tsx
@@ -8,5 +8,17 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          title={video.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      ))}
+    </div>
+  );
 }
