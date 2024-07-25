976082678f763ebb5e484e24e1f4f5bd63195ad7
watching-dev <coderefactoring90@gmail.com>
2024-07-25 11:51:16 +0900

    [feat] movie info 수정

--- NextJS14/app/(movies)/movies/[id]/page.tsx
+++ NextJS14/app/(movies)/movies/[id]/page.tsx
@@ -12,9 +12,9 @@ export default async function MovieDetail({
       <Suspense fallback={<h1>Loading..</h1>}>
         <MovieInfo id={id} />
       </Suspense>
-      <Suspense fallback={<h1>Loading..</h1>}>
+      {/* <Suspense fallback={<h1>Loading..</h1>}>
         <MovieVideos id={id} />
-      </Suspense>
+      </Suspense> */}
     </div>
   );
 }
--- NextJS14/components/movie-info.tsx
+++ NextJS14/components/movie-info.tsx
@@ -7,5 +7,14 @@ async function getMovie(id: string) {
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div>
+      <img src={movie.poster_path} />
+      <div>
+        <h1>{movie.title}</h1>
+        <h3>{movie.vote_average}</h3>
+        <p>{movie.overview}</p>
+      </div>
+    </div>
+  );
 }
