497e88f6d41932bc78e427b7f6cadc379c8cfa25
watching-dev <coderefactoring90@gmail.com>
2024-07-25 13:09:10 +0900

    [feat] movie video module css 설정

--- NextJS14/components/movie-videos.tsx
+++ NextJS14/components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id: string) {
   // throw new Error("error!");
@@ -9,7 +10,7 @@ async function getVideos(id: string) {
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
   return (
-    <div>
+    <div className={styles.container}>
       {videos.map((video) => (
         <iframe
           key={video.id}
