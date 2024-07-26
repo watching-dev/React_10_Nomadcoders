9f7713315990e10af5ed41f7c6e558d9a6fb8a4a
watching-dev <coderefactoring90@gmail.com>
2024-07-26 13:38:18 +0900

    [feat] 자식 선택자 *

--- NextJS-Master/carrot-market-reloaded/app/page.tsx
+++ NextJS-Master/carrot-market-reloaded/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2">
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none">
         <input
-          className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow ring-offset-blue-600 placeholder:text-red-600 placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+          className="w-full rounded-full h-10 bg-gray-200 pl-5 ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow ring-offset-blue-600 placeholder:text-red-600 placeholder:drop-shadow invalid:focus:ring-red-500 peer"
           type="text"
           required
           placeholder="Search"
@@ -11,7 +11,7 @@ export default function Home() {
         <span className="text-red-500 font-medium hidden peer-invalid:block">
           Email
         </span>
-        <button className=" text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none md:px-10 bg-black">
+        <button className=" text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium md:px-10 bg-black">
           Search
         </button>
       </div>
