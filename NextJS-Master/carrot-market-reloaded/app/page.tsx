337f4bc78306c79937e17f155e60970021c8041c
watching-dev <coderefactoring90@gmail.com>
2024-07-26 13:40:06 +0900

    [feat] 가상 클래스인 has는 자식이 .peer 가지고 있으면 작동

--- NextJS-Master/carrot-market-reloaded/app/page.tsx
+++ NextJS-Master/carrot-market-reloaded/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none">
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none has-[.peer]:bg-green-100">
         <input
           className="w-full rounded-full h-10 bg-gray-200 pl-5 ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow ring-offset-blue-600 placeholder:text-red-600 placeholder:drop-shadow invalid:focus:ring-red-500 peer"
           type="text"
