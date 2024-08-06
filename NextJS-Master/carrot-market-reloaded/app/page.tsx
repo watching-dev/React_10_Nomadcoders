f6a72bda2c88ff9ec682af748e22b0b349776ce9
watching-dev <coderefactoring90@gmail.com>
2024-08-06 22:27:10 +0900

    [feat] form 생성

--- NextJS-Master/carrot-market-reloaded/app/page.tsx
+++ NextJS-Master/carrot-market-reloaded/app/page.tsx
@@ -1,20 +1,22 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100 has-[:invalid]:ring">
-        <input
-          className="w-full rounded-full h-10 bg-gray-200 pl-5 ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow ring-offset-blue-600 placeholder:text-red-600 placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="text"
-          required
-          placeholder="Search"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          Email
-        </span>
-        <button className=" text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium md:px-10 bg-black">
-          Search
-        </button>
+    <>
+      <div className="flex flex-col gap-1 p-5">
+        <form className="flex flex-col gap-5">
+          <input
+            className="bg-transparent w-full h-10 rounded-md focus:outline-none ring-1"
+            type="email"
+            placeholder="email"
+            required
+          ></input>
+          <input type="text" placeholder="name" required></input>
+          <input type="password" placeholder="password" required></input>
+          <button className="w-full h-10 bg-gray-100 rounded-md font-semibold">
+            Log in
+          </button>
+        </form>
+        <div className="text-red-500 font-medium">text</div>
       </div>
-    </main>
+    </>
   );
 }
