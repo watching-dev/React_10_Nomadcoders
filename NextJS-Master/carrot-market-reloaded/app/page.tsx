160ecb0e65b698244357c3e7c31dee0a7dd707bb
watching-dev <coderefactoring90@gmail.com>
2024-08-06 23:18:37 +0900

    [feat] server action 추가

--- NextJS-Master/carrot-market-reloaded/app/page.tsx
+++ NextJS-Master/carrot-market-reloaded/app/page.tsx
@@ -3,17 +3,33 @@ import FormBtn from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 
 export default function Home() {
-  async function handleForm() {
+  async function handleForm(data: FormData) {
     "use server";
     console.log("---");
+    console.log(data.get("email"), data.get("name"), data.get("password"));
   }
   return (
     <>
       <div className="flex flex-col gap-1 p-5">
         <form action={handleForm} className="flex flex-col gap-5">
-          <FormInput type="email" placeholder="email" required={true} />
-          <FormInput type="text" placeholder="name" required={true} />
-          <FormInput type="password" placeholder="password" required={true} />
+          <FormInput
+            name="email"
+            type="email"
+            placeholder="email"
+            required={true}
+          />
+          <FormInput
+            name="name"
+            type="text"
+            placeholder="name"
+            required={true}
+          />
+          <FormInput
+            name="password"
+            type="password"
+            placeholder="password"
+            required={true}
+          />
           <FormBtn loading={false} />
           <FormErr errors={["wrong1, wrong2"]} />
         </form>
--- NextJS-Master/carrot-market-reloaded/components/form-input.tsx
+++ NextJS-Master/carrot-market-reloaded/components/form-input.tsx
@@ -2,11 +2,18 @@ interface IFormInput {
   type: string;
   placeholder: string;
   required: boolean;
+  name: string;
 }
 
-export default function FormInput({ type, placeholder, required }: IFormInput) {
+export default function FormInput({
+  type,
+  placeholder,
+  required,
+  name,
+}: IFormInput) {
   return (
     <input
+      name={name}
       className="bg-transparent w-full h-10 rounded-md focus:outline-none ring-1"
       type={type}
       placeholder={placeholder}
