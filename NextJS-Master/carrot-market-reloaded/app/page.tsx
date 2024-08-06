2e99093f6876dbc34d068229246d391608a46e5c
watching-dev <coderefactoring90@gmail.com>
2024-08-06 23:04:09 +0900

    [feat] action 추가

--- NextJS-Master/carrot-market-reloaded/app/page.tsx
+++ NextJS-Master/carrot-market-reloaded/app/page.tsx
@@ -2,10 +2,14 @@ import FormBtn from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 
 export default function Home() {
+  async function handleForm() {
+    "use server";
+    console.log("---");
+  }
   return (
     <>
       <div className="flex flex-col gap-1 p-5">
-        <form className="flex flex-col gap-5">
+        <form action={handleForm} className="flex flex-col gap-5">
           <FormInput type="email" placeholder="email" required={true} />
           <FormInput type="text" placeholder="name" required={true} />
           <FormInput type="password" placeholder="password" required={true} />
