79376f4c8d6a4e545b8dbfe3ef971ddbe29027e7
watching-dev <coderefactoring90@gmail.com>
2024-08-08 23:41:03 +0900

    [feat] prisma 설치 및 데이터베이스 설정

---
+++ NextJS-Master/carrot-market-reloaded/prisma/migrations/20240808143500_user/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "email" TEXT,
+    "password" TEXT,
+    "bio" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_bio_key" ON "User"("bio");
