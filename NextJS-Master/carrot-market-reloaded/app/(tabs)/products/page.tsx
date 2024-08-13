d2d57d98a373b4f8f9f6363cf37158ca31cd7da0
watching-dev <coderefactoring90@gmail.com>
2024-08-13 23:42:41 +0900

    [feat] auth tabs

---
+++ NextJS-Master/carrot-market-reloaded/app/(tabs)/products/page.tsx
@@ -0,0 +1,33 @@
+import ProductList from "@/components/product-list";
+import db from "@/lib/db";
+import { Prisma } from "@prisma/client";
+
+async function getInitialProducts() {
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+    take: 1,
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+  return products;
+}
+
+export type InitialProducts = Prisma.PromiseReturnType<
+  typeof getInitialProducts
+>;
+
+export default async function Products() {
+  const initialProducts = await getInitialProducts();
+  return (
+    <div>
+      <ProductList initialProducts={initialProducts} />
+    </div>
+  );
+}
