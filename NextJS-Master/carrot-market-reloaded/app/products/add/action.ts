7124ebdb0233028e5dd21fbbc05dddea8360d2f8
watching-dev <coderefactoring90@gmail.com>
2024-08-14 23:13:10 +0900

    [feat] lib

---
+++ NextJS-Master/carrot-market-reloaded/app/products/add/action.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+import { productSchema } from "./schema";
+
+export async function uploadProduct(formData: FormData) {
+  const data = {
+    photo: formData.get("photo"),
+    title: formData.get("title"),
+    price: formData.get("price"),
+    description: formData.get("description"),
+  };
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: result.data.photo,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+      //redirect("/products")
+    }
+  }
+}
+
+export async function getUploadUrl() {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CLOUDFLARE_API_KEY}`,
+      },
+    }
+  );
+  const data = await response.json();
+  return data;
+}
---
+++ NextJS-Master/carrot-market-reloaded/app/products/add/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import Button from "@/components/button";
+import Input from "@/components/input";
+import { PhotoIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import { getUploadUrl, uploadProduct } from "./actions";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ProductType, productSchema } from "./schema";
+
+export default function AddProduct() {
+  const [preview, setPreview] = useState("");
+  const [uploadUrl, setUploadUrl] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    setError,
+    formState: { errors },
+  } = useForm<ProductType>({
+    resolver: zodResolver(productSchema),
+  });
+  const onImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+    if (!files) {
+      return;
+    }
+    const file = files[0];
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+    setFile(file);
+    const { success, result } = await getUploadUrl();
+    if (success) {
+      const { id, uploadURL } = result;
+      setUploadUrl(uploadURL);
+      setValue(
+        "photo",
+        `https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${id}`
+      );
+    }
+  };
+  const onSubmit = handleSubmit(async (data: ProductType) => {
+    if (!file) {
+      return;
+    }
+    const cloudflareForm = new FormData();
+    cloudflareForm.append("file", file);
+    const response = await fetch(uploadUrl, {
+      method: "post",
+      body: cloudflareForm,
+    });
+    if (response.status !== 200) {
+      return;
+    }
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("price", data.price + "");
+    formData.append("description", data.description);
+    formData.append("photo", data.photo);
+    const errors = await uploadProduct(formData);
+    if (errors) {
+      // setError("")
+    }
+  });
+  const onValid = async () => {
+    await onSubmit();
+  };
+  return (
+    <div>
+      <form action={onValid} className="p-5 flex flex-col gap-5">
+        <label
+          htmlFor="photo"
+          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}
+        >
+          {preview === "" ? (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-neutral-400 text-sm">
+                사진을 추가해주세요.
+                {errors.photo?.message}
+              </div>
+            </>
+          ) : null}
+        </label>
+        <input
+          onChange={onImageChange}
+          type="file"
+          id="photo"
+          name="photo"
+          accept="image/*"
+          className="hidden"
+        />
+        <Input
+          required
+          placeholder="제목"
+          type="text"
+          {...register("title")}
+          errors={[errors.title?.message ?? ""]}
+        />
+        <Input
+          type="number"
+          required
+          placeholder="가격"
+          {...register("price")}
+          errors={[errors.price?.message ?? ""]}
+        />
+        <Input
+          type="text"
+          required
+          placeholder="자세한 설명"
+          {...register("description")}
+          errors={[errors.description?.message ?? ""]}
+        />
+        <Button text="작성 완료" />
+      </form>
+    </div>
+  );
+}
